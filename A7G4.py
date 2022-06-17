6b25d0bc1d8c070b4cf535db449f7870a681922c
romanzelararg <roman.zelar.arg@gmail.com>
2022-06-16 23:51:00 -0300

    commit app

---
+++ A7G4.py
@@ -0,0 +1,19 @@
+#Función que muestre cartel de presentación
+def cartel_presentacion():
+    saludo = ("Bienvenidos, somos del Grupo 7 Aula 4")
+    return saludo
+
+#función ing2i, debe permitir el ingreso de 2 valores enteros
+
+def ing2i ():
+    a= int( input("Ingrese el 1er número entero: "))
+    b= int( input("Ingrese el 2do número entero:  "))
+    return [a,b]
+
+#función ing2i, debe permitir el ingreso de 2 valores string
+
+def ing2i ():
+    palabra1= str( input("Ingrese el primer valor string: "))
+    palabra2= str( input("Ingrese el segundo valor string:  "))
+    return [palabra1,palabra2]
+
