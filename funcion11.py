ca4548fd9c0af465bf43f9d1b85eadf4afc0f166
romanzelararg <roman.zelar.arg@gmail.com>
2022-06-26 15:37:06 -0300

    commit funcion11

---
+++ funcion11.py
@@ -0,0 +1,9 @@
+#FUNCION 11- función p1, retorna la resta de los 2 primero por el 3er parámetros, usando las funciones anteriores.
+
+import resta
+
+def p1(a,b,c):
+   resta1 = resta.resta(a,b)
+   multip = resta1 * c
+   return (multip)
+
