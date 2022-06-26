2128ec08606c8fc40921f20ab253b8db58147a70
romanzelararg <roman.zelar.arg@gmail.com>
2022-06-26 13:59:11 -0300

    commit funcion9

---
+++ funcion9.py
@@ -0,0 +1,8 @@
+#FUNCION 9 - función p1, retorna el producto de los 2 primero más el 3er parámetros, usando las funciones anteriores.
+
+from producto import producto
+
+def funcion9(a,b,c):
+    return producto((a,b)+c)
+
+
